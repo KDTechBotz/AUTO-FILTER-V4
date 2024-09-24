24548ef38ba10e8f177feb3b6d778397c5aebef4
G0dAn-TG <crazyluttappi99@gmail.com>
2024-09-24 14:25:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -37,7 +37,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('⚡ Click Here For More Buttons ⚡', callback_data='start')
+                    InlineKeyboardButton('✨ Click Here For More Buttons ✨', callback_data='start')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -77,7 +77,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-                    InlineKeyboardButton('⚡ Click Here For More Buttons ⚡', callback_data='start')
+                    InlineKeyboardButton('✨ Click Here For More Buttons ✨', callback_data='start')
                     
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
